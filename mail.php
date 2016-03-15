9deb4fc9f047b6043542d3e72acfa68870a57954
Sahil <sahilgargin@gmail.com>
2016-03-15 08:10:16 -0400

    linked to social profile | Seo

--- mail.php
+++ mail.php
@@ -4,10 +4,12 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="Contact form\n\nFrom: $name \nMessage: $message";
-$recipient = "sahil@sahilgarg.in";
+$recipient = "sahilgargin@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You! E-mail send.";
 
